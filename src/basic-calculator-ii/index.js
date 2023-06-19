80f745f8ffe09cb0cee771816bea4643b9a04c3b
Dremy <dremy@dremy.cn>
2023-06-20 00:08:43 +0800

    :sparkles: 227. Basic Calculator II

--- src/basic-calculator-ii/index.js
+++ src/basic-calculator-ii/index.js
@@ -3,86 +3,31 @@
  * @return {number}
  */
 const calculate = (s) => {
-  let left1 = '';
-  let left2 = '';
-  let right = '';
-  let operate1 = null;
-  let operate2 = null;
-  for (let i = 0; i < s.length + 1; i++) {
+  const stack = [];
+  let buffer = '';
+  let operate = '+';
+  for (let i = 0; i <= s.length; i++) {
     if (s[i] === ' ') {
       continue;
     }
-    switch (s[i]) {
-      case '0':
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
-        if (!operate1) {
-          left1 += s[i];
-        } else {
-          right += s[i];
-        }
-        break;
-
-      case '+':
-      case '-':
-      case undefined:
-        if (operate2) {
-          right = calc(left2, operate2, right);
-          left2 = '';
-          operate2 = '';
-        }
-        if (operate1) {
-          left1 = calc(left1, operate1, right);
-          right = '';
-        }
-        operate1 = s[i];
-        break;
-
-      case '*':
-      case '/':
-        if (operate2) {
-          left2 = calc(left2, operate2, right);
-          right = '';
-          operate2 = s[i];
-        } else if (operate1) {
-          if (operate1 === '+' || operate1 === '-') {
-            operate2 = s[i];
-            left2 = right;
-            right = '';
-          } else {
-            left1 = calc(left1, operate1, right);
-            right = '';
-            operate1 = s[i];
-          }
-        } else {
-          operate1 = s[i];
-        }
-        break;
+    if (['+', '-', '*', '/'].includes(s[i]) || i === s.length) {
+      if (operate === '+') {
+        stack.push(+buffer);
+      } else if (operate === '-') {
+        stack.push(-buffer);
+      } else if (operate === '*') {
+        stack.push(stack.pop() * buffer);
+      } else {
+        stack.push(Math.trunc(stack.pop() / buffer));
+      }
+      operate = s[i];
+      buffer = '';
+    } else {
+      buffer += s[i];
     }
   }
-  return left1;
-};
 
-/**
- */
-function calc(left, operator, right) {
-  switch (operator) {
-    case '+':
-      return Number(left) + Number(right);
-    case '-':
-      return Number(left) - Number(right);
-    case '*':
-      return Number(left) * Number(right);
-    case '/':
-      return Math.floor(Number(left) / Number(right));
-  }
-}
+  return stack.reduce((total, num) => total + num, 0);
+};
 
 export default calculate;
